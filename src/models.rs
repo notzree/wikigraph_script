d00c12d92591f5980bf8f7b5e0da74512303ed2e
notzree <notzree@gmail.com>
2024-03-06 10:48:10 -0500

    Better diesel error messages

--- src/models.rs
+++ src/models.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 
 #[derive(Insertable, Queryable, Serialize, Debug)]
 #[table_name = "lookup"]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct LookupEntry {
     pub title: String,
     pub byteoffset: i32,
@@ -12,6 +13,7 @@ pub struct LookupEntry {
 
 #[derive(Insertable, Queryable, Serialize, Debug)]
 #[table_name = "redirect"]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct RedirectEntry {
     pub redirect_from: String,
     pub redirect_to: String,
