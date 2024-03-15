6c28f99ece11d009f5974ba6adce3343cc1cad72
notzree <notzree@gmail.com>
2024-03-15 18:35:27 -0400

    Update driver code

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,17 @@ use parser::Parser;
 extern crate chrono;
 use std::time::Instant;
 
-const FILE_PATH: &str = "raw_data/enwiki-latest-pages-articles.xml";
+use crate::{database_handler::DatabaseHandler, graph_builder::GraphBuilder};
+
+const FILE_PATH: &str = "raw_data/enwiki-pages-articles.xml";
 const BINARY_GRAPH_PATH: &str = "raw_data/binary_graph.bin";
 const VERSION: i32 = 1;
 const ADJ_LIST_PATH: &str = "adjacency_list.txt";
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_handler = database_handler::PostgresDatabaseHandler::new(&db_url)?;
+    let mut database_handler = database_handler::PostgresDatabaseHandler::new(&db_url)?;
     let graph_builder =
-        graph_builder::WikiBinaryGraphBuilder::new(BINARY_GRAPH_PATH.to_owned(), 0, VERSION);
+        graph_builder::WikiBinaryGraphBuilder::new(BINARY_GRAPH_PATH.to_owned(), 9030425, VERSION);
     let link_handler = link_handler::WikiLinkHandler;
     let adj_list_handler = adj_list_handler::WikigraphAdjacencyListHandler::new(ADJ_LIST_PATH);
     let mut parser = Parser::new(
@@ -29,10 +31,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         adj_list_handler,
         graph_builder,
     );
-    let start = Instant::now();
-    parser.pre_process_file().unwrap();
-    let duration = start.elapsed();
-    println!("Graph creation time: {:?}", duration);
+    //Rerun this later
+    let mut start = Instant::now();
+    // parser.pre_process_file().unwrap();
+    // println!("Pre-process time: {:?}", start.elapsed());
+    start = Instant::now();
+    parser.create_graph();
+    println!("Graph creation time: {:?}", start.elapsed());
 
     Ok(())
 }
